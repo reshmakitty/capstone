06081677e3f6245da8a621b37e3bf31ae7aa97a1
reshmakitty <98320714+reshmakitty@users.noreply.github.com>
2022-06-26 19:55:18 +0530

    Add files via upload

---
+++ InitialData.java
@@ -0,0 +1,21 @@
+package com.MyMoviePlan.config;
+
+import com.MyMoviePlan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitialData implements CommandLineRunner {
+
+    @Autowired
+    private UserService service;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
